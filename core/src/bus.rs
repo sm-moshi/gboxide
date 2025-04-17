818a763f4f85bf6e0b370acfe5da82b61a5007cb
sm_moshi <stuartmeya@proton.me>
2025-04-17 20:50:23 +0200

    Update bus.rs

--- core/src/bus.rs
+++ core/src/bus.rs
@@ -4,13 +4,11 @@ use crate::mmu::MMU;
 pub trait MemoryBusTrait {
     fn read8(&mut self, addr: u16) -> u8;
     fn write8(&mut self, addr: u16, value: u8);
-
     fn read16(&mut self, addr: u16) -> u16 {
         let lo = self.read8(addr) as u16;
         let hi = self.read8(addr.wrapping_add(1)) as u16;
         (hi << 8) | lo
     }
-
     fn write16(&mut self, addr: u16, value: u16) {
         let lo = value as u8;
         let hi = (value >> 8) as u8;
