1b821821880958303b3921190590f29949ea5c6f
sm_moshi <stuartmeya@proton.me>
2025-04-17 20:35:51 +0200

    Update bus.rs

--- core/src/bus.rs
+++ core/src/bus.rs
@@ -1,14 +1,16 @@
 /// Memory interface abstraction between CPU and memory/IO
 use crate::mmu::MMU;
 
-pub trait MemoryBus {
+pub trait MemoryBusTrait {
     fn read8(&mut self, addr: u16) -> u8;
     fn write8(&mut self, addr: u16, value: u8);
+
     fn read16(&mut self, addr: u16) -> u16 {
         let lo = self.read8(addr) as u16;
         let hi = self.read8(addr.wrapping_add(1)) as u16;
         (hi << 8) | lo
     }
+
     fn write16(&mut self, addr: u16, value: u16) {
         let lo = value as u8;
         let hi = (value >> 8) as u8;
@@ -17,23 +19,22 @@ pub trait MemoryBus {
     }
 }
 
-// Rename the wrapper struct to avoid conflict:
-pub struct MMUBus<'a> {
+pub struct MemoryBus<'a> {
     pub mmu: &'a mut MMU,
 }
 
-impl<'a> MMUBus<'a> {
+impl<'a> MemoryBus<'a> {
     pub fn new(mmu: &'a mut MMU) -> Self {
         Self { mmu }
     }
 }
 
-impl<'a> MemoryBus for MMUBus<'a> {
+impl<'a> MemoryBusTrait for MemoryBus<'a> {
     fn read8(&mut self, addr: u16) -> u8 {
-        self.mmu.read8(addr)
+        self.mmu.read(addr)
     }
 
     fn write8(&mut self, addr: u16, value: u8) {
-        self.mmu.write8(addr, value);
+        self.mmu.write(addr, value);
     }
 }
