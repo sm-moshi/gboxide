e8191071d1aa759d7dee59ffeeccbd5427b62b55
sm_moshi <stuartmeya@proton.me>
2025-04-17 20:13:24 +0200

    Update bus.rs

--- core/src/bus.rs
+++ core/src/bus.rs
@@ -17,33 +17,23 @@ pub trait MemoryBus {
     }
 }
 
-pub struct MemoryBus<'a> {
+// Rename the wrapper struct to avoid conflict:
+pub struct MMUBus<'a> {
     pub mmu: &'a mut MMU,
 }
 
-impl<'a> MemoryBus<'a> {
+impl<'a> MMUBus<'a> {
     pub fn new(mmu: &'a mut MMU) -> Self {
         Self { mmu }
     }
+}
 
-    pub fn read8(&mut self, addr: u16) -> u8 {
+impl<'a> MemoryBus for MMUBus<'a> {
+    fn read8(&mut self, addr: u16) -> u8 {
         self.mmu.read8(addr)
     }
 
-    pub fn write8(&mut self, addr: u16, value: u8) {
+    fn write8(&mut self, addr: u16, value: u8) {
         self.mmu.write8(addr, value);
-    }
-
-    pub fn read16(&mut self, addr: u16) -> u16 {
-        let lo = self.read8(addr) as u16;
-        let hi = self.read8(addr.wrapping_add(1)) as u16;
-        (hi << 8) | lo
-    }
-
-    pub fn write16(&mut self, addr: u16, value: u16) {
-        let lo = value as u8;
-        let hi = (value >> 8) as u8;
-        self.write8(addr, lo);
-        self.write8(addr.wrapping_add(1), hi);
     }
 }
