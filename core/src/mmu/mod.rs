1f684ea4a7bb7a9574667f25a52c85e64e2076e4
sm_moshi <stuartmeya@proton.me>
2025-04-17 21:18:08 +0200

    Update mod.rs

--- core/src/mmu/mod.rs
+++ core/src/mmu/mod.rs
@@ -1,79 +1,61 @@
-use crate::bus::Bus;
+use crate::cartridge::Cartridge;
+use crate::interrupts::InterruptHandler;
 
+/// Memory Management Unit implementation.
 pub struct MMU {
     rom: Vec<u8>,
-    ram: Vec<u8>,
-    rom_bank: usize,
-    ram_bank: usize,
-    ram_enabled: bool,
+    vram: [u8; 0x2000],
+    eram: [u8; 0x2000],
+    wram: [u8; 0x2000],
+    oam: [u8; 0xA0],
+    io: [u8; 0x80],
+    hram: [u8; 0x7F],
+    ie: u8,
+    interrupt_handler: InterruptHandler,
 }
 
 impl MMU {
-    pub fn new() -> Self {
+    pub fn new(cartridge: Cartridge) -> Self {
         Self {
-            rom: vec![0; 0x8000],
-            ram: vec![0; 0x8000],
-            rom_bank: 1,
-            ram_bank: 0,
-            ram_enabled: false,
+            rom: cartridge.rom,
+            vram: [0; 0x2000],
+            eram: [0; 0x2000],
+            wram: [0; 0x2000],
+            oam: [0; 0xA0],
+            io: [0; 0x80],
+            hram: [0; 0x7F],
+            ie: 0,
+            interrupt_handler: InterruptHandler::default(),
         }
-    }
-
-    pub fn load_rom(&mut self, data: Vec<u8>) {
-        self.rom = data;
     }
 
     pub fn read(&self, addr: u16) -> u8 {
         match addr {
-            0x0000..=0x3FFF => self.rom[addr as usize],
-            0x4000..=0x7FFF => {
-                let offset = self.rom_bank * 0x4000;
-                self.rom[offset + (addr as usize - 0x4000)]
-            }
-            0xA000..=0xBFFF => {
-                if self.ram_enabled {
-                    let offset = self.ram_bank * 0x2000;
-                    self.ram[offset + (addr as usize - 0xA000)]
-                } else {
-                    0xFF
-                }
-            }
+            0x0000..=0x7FFF => self.rom[addr as usize],
+            0x8000..=0x9FFF => self.vram[(addr - 0x8000) as usize],
+            0xA000..=0xBFFF => self.eram[(addr - 0xA000) as usize],
+            0xC000..=0xDFFF => self.wram[(addr - 0xC000) as usize],
+            0xE000..=0xFDFF => self.wram[(addr - 0xE000) as usize],
+            0xFE00..=0xFE9F => self.oam[(addr - 0xFE00) as usize],
+            0xFF00..=0xFF7F => self.io[(addr - 0xFF00) as usize],
+            0xFF80..=0xFFFE => self.hram[(addr - 0xFF80) as usize],
+            0xFFFF => self.ie,
             _ => 0xFF,
         }
     }
 
     pub fn write(&mut self, addr: u16, value: u8) {
         match addr {
-            0x0000..=0x1FFF => {
-                self.ram_enabled = (value & 0x0F) == 0x0A;
-            }
-            0x2000..=0x3FFF => {
-                let bank = (value as usize) & 0x1F;
-                self.rom_bank = if bank == 0 { 1 } else { bank };
-            }
-            0x4000..=0x5FFF => {
-                self.ram_bank = (value as usize) & 0x03;
-            }
-            0x6000..=0x7FFF => {
-                // Banking mode switch (ignored)
-            }
-            0xA000..=0xBFFF => {
-                if self.ram_enabled {
-                    let offset = self.ram_bank * 0x2000;
-                    self.ram[offset + (addr as usize - 0xA000)] = value;
-                }
-            }
+            0x0000..=0x7FFF => { /* ROM is read-only */ }
+            0x8000..=0x9FFF => self.vram[(addr - 0x8000) as usize] = value,
+            0xA000..=0xBFFF => self.eram[(addr - 0xA000) as usize] = value,
+            0xC000..=0xDFFF => self.wram[(addr - 0xC000) as usize] = value,
+            0xE000..=0xFDFF => self.wram[(addr - 0xE000) as usize] = value,
+            0xFE00..=0xFE9F => self.oam[(addr - 0xFE00) as usize] = value,
+            0xFF00..=0xFF7F => self.io[(addr - 0xFF00) as usize] = value,
+            0xFF80..=0xFFFE => self.hram[(addr - 0xFF80) as usize] = value,
+            0xFFFF => self.ie = value,
             _ => {}
         }
-    }
-}
-
-impl Bus for MMU {
-    fn read8(&mut self, addr: u16) -> u8 {
-        self.read(addr)
-    }
-
-    fn write8(&mut self, addr: u16, value: u8) {
-        self.write(addr, value)
     }
 }
